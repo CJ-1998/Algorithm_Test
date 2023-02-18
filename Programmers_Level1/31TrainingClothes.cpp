87e69d8a8f4028aa8c872689286918bd64cb6e39
CJ <choijun0627@konkuk.ac.kr>
2023-02-18 09:10:14 +0900

    <feat> 프로그래머스 level 1 1문제 풀이
    31번 체육복 풀이 완료

---
+++ Programmers_Level1/31TrainingClothes.cpp
@@ -0,0 +1,58 @@
+/*
+문제:
+    점심시간에 도둑이 들어, 일부 학생이 체육복을 도난당했습니다. 다행히 여벌 체육복이 있는 학생이 이들에게 체육복을 빌려주려 합니다. 학생들의 번호는 체격 순으로 매겨져 있어, 바로 앞번호의 학생이나 바로 뒷번호의 학생에게만 체육복을 빌려줄 수 있습니다. 예를 들어, 4번 학생은 3번 학생이나 5번 학생에게만 체육복을 빌려줄 수 있습니다. 체육복이 없으면 수업을 들을 수 없기 때문에 체육복을 적절히 빌려 최대한 많은 학생이 체육수업을 들어야 합니다.
+    전체 학생의 수 n, 체육복을 도난당한 학생들의 번호가 담긴 배열 lost, 여벌의 체육복을 가져온 학생들의 번호가 담긴 배열 reserve가 매개변수로 주어질 때, 체육수업을 들을 수 있는 학생의 최댓값을 return 하도록 solution 함수를 작성해주세요.
+
+제한 사항:
+    전체 학생의 수는 2명 이상 30명 이하입니다.
+    체육복을 도난당한 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
+    여벌의 체육복을 가져온 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
+    여벌 체육복이 있는 학생만 다른 학생에게 체육복을 빌려줄 수 있습니다.
+    여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다. 이때 이 학생은 체육복을 하나만 도난당했다고 가정하며, 남은 체육복이 하나이기에 다른 학생에게는 체육복을 빌려줄 수 없습니다.
+*/
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    int answer = 0;
+    
+    sort(lost.begin(),lost.end());
+    sort(reserve.begin(),reserve.end());
+    
+    for(int i=0;i<lost.size();i++){
+        for(int j=0;j<reserve.size();j++){
+            if(lost[i]==reserve[j]){
+                lost.erase(lost.begin()+i);
+                reserve.erase(reserve.begin()+j);
+                i--;
+                j--;
+                break;
+            }
+        }
+    }
+    
+    for(int i=0;i<lost.size();i++){
+        for(int j=0;j<reserve.size();j++){
+             if(lost[i]==reserve[j]-1||lost[i]==reserve[j]+1){
+                lost.erase(lost.begin()+i);
+                reserve.erase(reserve.begin()+j);
+                i--;
+                j--;
+                break;
+            }      
+        }
+    }
+    
+    answer=n-lost.size();
+    
+    return answer;
+}
+
+/*
+배운 점:
+    1. 벡터의 i index의 원소 지우기 : vec.erase(vec.begin()+i); 이렇게하면 i index의 값은 뒤의 있는 것으로 채워진다. 즉 뒤의 원소들이 한 칸씩 앞으로 당겨진다. 
+*/
