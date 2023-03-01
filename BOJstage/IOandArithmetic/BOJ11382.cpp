6d986056894f5093a6825eda8f1690acf1702fa6
CJ <choijun0627@konkuk.ac.kr>
2023-03-01 11:06:13 +0900

    <feat> 백준 단계별로 풀어보기 폴더 생성, 3문제 풀이
    백준 단계별로 풀어보기에서 안 푼 문제 순서대로 푸는 것이다
    
    1단계 입출력과 사칙연산 11382번 꼬마 정민 풀이 완료
    
    3단계 반복문 25304번 영수증 풀이 완료
    3단계 반복문 25314번 코딩은 체육과목입니다 풀이 완료

---
+++ BOJstage/IOandArithmetic/BOJ11382.cpp
@@ -0,0 +1,26 @@
+/*
+제목: 
+    꼬마 정민
+
+문제:
+    꼬마 정민이는 이제 A + B 정도는 쉽게 계산할 수 있다. 이제 A + B + C를 계산할 차례이다!
+
+입력:
+    첫 번째 줄에 A, B, C (1 ≤ A, B, C ≤ 10^12)이 공백을 사이에 두고 주어진다.
+
+출력:
+    A+B+C의 값을 출력한다.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    long long a, b, c;
+    long long sum;
+    cin>>a>>b>>c;
+    sum=a+b+c;
+    cout<<sum<<endl;
+    return 0;
+}
