97dd86891af7956dafe21720bdba75bf1e4a9ba0
CJ <choijun0627@konkuk.ac.kr>
2023-03-11 12:07:15 +0900

    <feat> 프로그래머스 level 1 1문제 풀이
    64번 나누어 떨어지는 숫자 배열 풀이 완료

---
+++ Programmers_Level1/63SumBetweenTwoNum.cpp
@@ -0,0 +1,40 @@
+/*
+문제:
+    두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.
+    예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
+
+제한 사항:
+    a와 b가 같은 경우는 둘 중 아무 수나 리턴하세요.
+    a와 b는 -10,000,000 이상 10,000,000 이하인 정수입니다.
+    a와 b의 대소관계는 정해져있지 않습니다.
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    int big, small;
+    if(a==b){
+        return a;
+    }
+    else if(a<b){
+        big=b;
+        small=a;
+    }
+    else{
+        big=a;
+        small=b;
+    }
+    for(int i=small;i<=big;i++){
+        answer+=i;
+    }
+    return answer;
+}
+
+/*
+개선 사항:
+    1.시그마 공식을 사용하면 된다. 
+*/
