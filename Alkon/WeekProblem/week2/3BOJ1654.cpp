74f51bf4abdc39c8a6fcbcb5f55ba7425ae2b4a7
CJ <choijun0627@konkuk.ac.kr>
2023-03-29 23:38:11 +0900

    <feat> Alkon 2주차 1문제 풀이
    BOJ 1654번 랜선 자르기 풀이 완료

--- Alkon/WeekProblem/week2/3BOJ1654.cpp
+++ Alkon/WeekProblem/week2/3BOJ1654.cpp
@@ -1,5 +1,4 @@
 /*
-(아직 못 품)
 문제:
     집에서 시간을 보내던 오영식은 박성원의 부름을 받고 급히 달려왔다. 박성원이 캠프 때 쓸 N개의 랜선을 만들어야 하는데 너무 바빠서 영식이에게 도움을 청했다.
     이미 오영식은 자체적으로 K개의 랜선을 가지고 있다. 그러나 K개의 랜선은 길이가 제각각이다. 박성원은 랜선을 모두 N개의 같은 길이의 랜선으로 만들고 싶었기 때문에 K개의 랜선을 잘라서 만들어야 한다. 예를 들어 300cm 짜리 랜선에서 140cm 짜리 랜선을 두 개 잘라내면 20cm는 버려야 한다. (이미 자른 랜선은 붙일 수 없다.)
@@ -22,102 +21,55 @@ int main(){
     long long answer=0;
 
     int k=0;
+    int n=0;
+
     scanf("%d",&k);
-    //가지고 있는 랜선 개수 k 받음
+    scanf("%d",&n);
 
-    long long n=0;
-    scanf("%lld", &n);
-    //필요한 랜선 개수 n 받음
-
-    vector<long long> v;
-    //랜선 길이 저장할 벡터 v
-
+    vector<int> v;
     for(int i=0;i<k;i++){
-        long long temp=0;
-        scanf("%lld",&temp);
+        int temp=0;
+        scanf("%d",&temp);
         v.push_back(temp);
     }
-    //벡터에 랜선 길이 받아서 저장
 
     sort(v.begin(),v.end());
-    //랜선 길이 오름차순으로 정렬
+
+    long long small=v[0];
+    long long big=v[v.size()-1];
+
+    //printf("%lld %lld\n",small, big);
 
     long long start=1;
-    long long end=v[0];
-    long long mid=end;
-
-    long long num=0;
-    for(int i=0;i<k;i++){
-        num+=v[i]/mid;
-    }
-    //현재 최소 랜선 길이로 랜선 나눴을 때 개수 num에 저장
-
-    if(num==n){
-        answer=mid;
-        printf("%lld",answer);
-
-        return 0;
-    }
-    //최소 랜선 길이로 나눴을 때 개수가 필요한 개수면 정답
+    long long end=big;
 
     while(start<=end){
-        num=0;
-        mid=(start+end)/2;
-        for(int i=0;i<k;i++){
-            num+=v[i]/mid;
+        //printf("%d %lld %lld\n", answer, start, end);
+        long long sum=0;
+        long long mid=(start+end)/2;
+        for(int i=0;i<v.size();i++){
+            long long temp=(long long)v[i]/mid;
+            sum+=temp;
         }
-        if(num==n){
+
+        if(sum>=n){
             answer=mid;
-            break;
+            start=mid+1;
+            // break;
         }
-        else 
-        {
-            if(num>n){
-                start=mid+1;
-            }
-            else{
-                end=mid-1;
-            }
+        else if(sum>n){
+            start=mid+1;
+        }
+        else{
+            end=mid-1;
         }
     }
-
-    start=mid+1;
-    end=v[v.size()-1];
-    mid=end;
-
-    num=0;
-    for(int i=0;i<k;i++){
-        num+=v[i]/mid;
-    }
-    if(num==n){
-        answer=mid;
-        printf("%lld",answer);
-
-        return 0;
-    }
-
-    while(start<=end){
-        num=0;
-        mid=(start+end)/2;
-        for(int i=0;i<k;i++){
-            num+=v[i]/mid;
-        }
-        if(num==n){
-            answer=mid;
-            break;
-        }
-        else 
-        {
-            if(num>n){
-                start=mid+1;
-            }
-            else{
-                end=mid-1;
-            }
-        }
-    }
-
     printf("%lld",answer);
 
     return 0;
 }
+
+/*
+배운 점:
+    1. 매개변수 탐색은 같으면 끝내는 것이 아니고 start를 계속 가면서 찾는 것 같다. 
+*/
