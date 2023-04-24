0a36ee5b7a436a1f8873728459897ee76db64fe4
CJ <choijun0627@konkuk.ac.kr>
2023-04-24 23:17:30 +0900

    <feat> Do it! 알고리즘 코딩 테스트 2문제 풀이
    4번 BOJ 11660번 구간 합 구하기 2  풀이 완료

--- Do_It!_Algorithm_Coding_Test_C++/4_BOJ11660.cpp
+++ Do_It!_Algorithm_Coding_Test_C++/4_BOJ11660.cpp
@@ -20,42 +20,35 @@
 */
 
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
-int main(){
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	int N, M;
+	cin >> N >> M;
+	vector<vector<int>> A(N + 1, vector<int>(N + 1, 0));
+	vector<vector<int>> D(N + 1, vector<int>(N + 1, 0));
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 1; j <= N; j++)
+		{
+			cin >> A[i][j];
+			// 구간 합 구하기
+			D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + A[i][j];
+		}
+	}
 
-    int arrsize=0;
-    int cnt=0;
-    cin>>arrsize>>cnt;
-
-    int **arr=new int* [arrsize];
-
-    for(int i=0;i<arrsize;i++){
-        int* temp = new int[arrsize];
-        for(int j=0;j<arrsize;j++){
-            cin>>temp[j];
-        }
-        arr[i]=temp;
-    }
-
-    for(int i=0;i<cnt;i++){
-        int x1=0,y1=0,x2=0,y2=0;
-        int answer=0;
-
-        cin>>x1>>y1>>x2>>y2;
-
-        for(int j=x1-1;j<=x2-1;j++){
-            for(int k=y1-1;k<=y2-1;k++){
-                answer+=arr[j][k];
-            }
-        }
-        cout<<answer<<"\n";
-    }
-
-    return 0;
+	for (int i = 0; i < M; i++)
+	{
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+		// 구간 합 배열로 질의에 답변하기
+		int result = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1];
+		cout << result << "\n";
+	}
 }
