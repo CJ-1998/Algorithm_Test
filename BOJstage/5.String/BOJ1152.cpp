2eaa6092c30c542ace2b84345e8a3d66ba780074
CJ <choijun0627@konkuk.ac.kr>
2023-05-03 21:01:40 +0900

    <feat> 백준 단계별로 풀어보기 문자열 1문제 풀이
    5단계 문자열 BOJ 1152번 단어의 개수 풀이 완료

--- BOJstage/5.String/BOJ1152.cpp
+++ BOJstage/5.String/BOJ1152.cpp
@@ -1,3 +1,19 @@
+/*
+문제:
+    영어 대소문자와 공백으로 이루어진 문자열이 주어진다. 이 문자열에는 몇 개의 단어가 있을까? 이를 구하는 프로그램을 작성하시오. 
+    단, 한 단어가 여러 번 등장하면 등장한 횟수만큼 모두 세어야 한다.
+
+입력:
+    첫 줄에 영어 대소문자와 공백으로 이루어진 문자열이 주어진다. 
+    이 문자열의 길이는 1,000,000을 넘지 않는다. 
+    단어는 공백 한 개로 구분되며, 공백이 연속해서 나오는 경우는 없다. 
+    또한 문자열은 공백으로 시작하거나 끝날 수 있다.
+
+출력:
+    첫째 줄에 단어의 개수를 출력한다.
+
+*/
+
 #include <iostream>
 #include <string>
 
@@ -7,24 +23,29 @@ int main(){
     string str;
     int answer=0;
 
-    while(str.compare("\n")){
-        cin>>str;
-        answer++;
+    getline(cin,str);
+
+    for(int i=0;i<str.size();i++){
+        if(str[i]==' '){
+            answer++;
+        }
     }
 
+    if(str[0]==' ')
+        answer--;
 
-    // for(int i=0;i<str.size();i++){
-    //     if('a'<=str[i]<='z'||'A'<=str[i]<='Z'){
-    //     //if(str[i]==' '){
-    //         answer++;
-    //     }
-    // }
-
-    // answer=str.size()-answer;
-    // if('a'<=str[0]<='z'||'A'<=str[0]<='Z')
-    //     answer++;
+    if(str[str.size()-1]==' ' )
+        answer--;
+       
     
+    answer++;
     cout<<answer<<endl;
 
     return 0;
 }
+
+/*
+배운 점:
+    1. 공백 포함 문자열을 입력 받으려면 getlin(cin, string 변수); 사용하면 된다. 
+    2. 또는 cin.getline(char 배열, 배열 길이); 사용하면 된다.
+*/
