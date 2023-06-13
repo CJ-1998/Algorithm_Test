aea2307504e7ad0226fc56e5aff53bc6ad279590
CJ <choijun0627@konkuk.ac.kr>
2023-06-13 14:29:15 +0900

    <feat> 프로그래머스 level 2 1문제 풀이
    최댓값과 최솟값 문제 풀이 완료

--- Alkon/WeekProblem/week7/BOJ1753.cpp
+++ Alkon/WeekProblem/week7/BOJ1753.cpp
@@ -1,30 +1,88 @@
 #include <cstdio>
-#include <vector>
 #include <utility>
+#include <vector>
+
+#define INF 999999999
 
 using namespace std;
 
+int v,e;
+int start;
+vector<pair<int,int>>* arr;
+int* dist;
+bool* visited;
+
+int small(){
+    int min=INF;
+    int index=0;
+
+    for(int i=1;i<=v;i++){
+        if(dist[i]<min&&!visited[i]){
+            min=dist[i];
+            index=i;
+        }
+    }
+    return index;
+}
+
+void dijkstra(int start){
+    for(int i=0;i<arr[start].size();i++){
+        dist[arr[start][i].first]=arr[start][i].second;
+    }
+
+    dist[start]=0;
+    visited[start]=true;
+
+    for(int i=0;i<v-1;i++){
+        int s=small();
+        visited[s]=true;
+
+        for(int i=0;i<arr[s].size();i++){
+            if(dist[arr[s][i].first]>dist[s]+arr[s][i].second){
+                dist[arr[s][i].first]=dist[s]+arr[s][i].second;
+            }
+        }
+    }
+
+}
+
+
 int main(){
-    int v,e;
-    scanf("%d %d",&v,&e);
 
-    int startnode;
-    scanf("%d",&startnode);
+    scanf("%d %d %d", &v, &e, &start);
+    dist=new int[v+1];
+    visited=new bool[v+1];
 
-    vector<pair<int,int>>* arr=new vector<pair<int,int>>[v+1];
+    for(int i=1;i<=v;i++){
+        dist[i]=INF;
+        visited[i]=false;
+    }
+
+    arr=new vector<pair<int,int>>[v+1];
 
     for(int i=1;i<=v;i++){
         vector<pair<int,int>> temp;
         arr[i]=temp;
     }
 
-    for(int i=0;i<e;i++){
-        int start,end,a;
-        scanf("%d %d %d",&start,&end,&a);
 
-        pair<int,int> p;
-        p=make_pair(end,a);
-        arr[start].push_back(p);
+    for(int i=0;i<e;i++){
+        int fir, sec, wei;
+        scanf("%d %d %d", &fir, &sec, &wei);
+
+        pair<int,int> temp;
+        temp=make_pair(sec,wei);
+
+        arr[fir].push_back(temp);
+    }
+
+    dijkstra(start);
+
+    for(int i=1;i<=v;i++){
+        if(dist[i]==INF)
+            printf("INF\n");
+        else
+            printf("%d\n",dist[i]);
     }
 
     return 0;
