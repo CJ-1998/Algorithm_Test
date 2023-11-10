dc3c932677dd38bc95e98f1c1bb1ec0b25668f8b
CJ <choijun0627@konkuk.ac.kr>
2023-11-10 23:56:55 +0900

    Feat : SWEA D3 문제 풀이
    1206번 view 문제 풀이 완료

---
+++ SWEA/D3/1206View.cpp
@@ -0,0 +1,20 @@
+
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+
+	cin>>T;
+	/*
+	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+	*/
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
