09cd1aaea61279c3695b3d05225dc37e5e758d35
CJ <choijun0627@konkuk.ac.kr>
2023-12-11 23:41:10 +0900

    Fix : Alkon 1주차 문제 풀이 중
    BOj 2065번 나룻배 문제 풀이 중

--- Alkon/WeekProblem/week1/4BOJ2065.cpp
+++ Alkon/WeekProblem/week1/4BOJ2065.cpp
@@ -1,3 +1,14 @@
+/*
+요구사항:
+    1. 나룻배는 처음에 왼쪽에 정박해 있다. 
+    2. 나룻배는 한번에 최대 M명 사람 태울 수 있다. 
+    3. 이동할 때 t 시간 걸린다. 
+    4. 정박장에 기다리고 있던 손님 배에 태울 수 있는데까지 태운다. 
+    5. 기다리고 있는 손님 없으면 그 정박장에서 기다린다. 
+    6. 기다리다가 반대편 정박장에 손님 오면 그 정박장으로 이동한다. 
+    7. 손님들이 원하는 곳에 도착하게 되는 시간 구하자. 
+*/
+
 #include <cstdio>
 #include <iostream>
 #include <queue>
@@ -7,159 +18,68 @@
 using namespace std;
 
 int main(){
-    int ship=0;
-    int t=0;
-    int n=0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(false);
+    cout.tie(false);
 
-    scanf("%d %d %d",&ship, &t, &n);
+    const string LEFT="left";
+    const string RIGHT="right";
 
-    queue<int> left;
-    queue<int> right;
+    int maxPerson=0;
+    int movingTime=0;
+    int num=0;
 
-    for(int i=0;i<n;i++){
+    cin>>maxPerson>>movingTime>>num;
+
+    vector<int> arrivalTime;
+    vector<string> arrivalPlace;
+    queue<int> arrivalLeft;
+    queue<int> arrivalRight;
+
+    for(int i=0;i<num;i++){
         int time=0;
-        string lr;
+        string place="";
 
-        //scanf("%d %s", &time, &lr);
+        cin>>time>>place;
 
-        cin>>time>>lr;
-
-        if(!lr.compare("left")){
-            left.push(time);
+        if(place.compare(LEFT)==0){
+            arrivalLeft.push(time);
+        }
+        else{
+            arrivalRight.push(time);
         }
 
-        if(!lr.compare("right")){
-            right.push(time);
-        }
+        arrivalTime.push_back(time);
+        arrivalPlace.push_back(place);
     }
 
-    int nowtime=0;
-    string nowship="left";
+    int nowTime=0;
+    string nowPlace=LEFT;
 
-    vector<int> arrivetime;
+    int cnt=0;
 
     while(true){
-        if(left.empty()&&right.empty()){
+
+        if(cnt==num){
             break;
         }
-        int nowshipcnt=0;
-        if(!nowship.compare("left")){
-            if(!left.empty()&&!right.empty()){
-                while(nowtime>=left.front()){
-                    if(nowshipcnt==ship){
-                        break;
-                    }
-                    left.pop();
-                    nowshipcnt++;
-                    if(left.empty())
-                        break;
-                }
-                if(nowshipcnt==0){
-                    if(left.front()>right.front()){
-                        nowtime=right.front();
-                        nowtime+=t;
-                        nowship="right";
-                    }
-                    else{
-                        nowtime=left.front();
-                    }
-                }
-                else{
-                    nowtime+=t;
-                    nowship="right";
-                    for(int i=0;i<nowshipcnt;i++){
-                        arrivetime.push_back(nowtime);
-                    }
+
+        if(nowPlace.compare(LEFT)==0){
+            if(arrivalLeft.front()<=nowTime){
+                
+            }
+            else{
+                if(arrivalLeft.front()<arrivalRight.front()){
+                    
                 }
             }
-            else if(!left.empty()&&right.empty()){
-                while(nowtime>=left.front()){
-                    if(nowshipcnt==ship){
-                        break;
-                    }
-                    left.pop();
-                    nowshipcnt++;
-                    if(left.empty())
-                        break;
-                }
-                if(nowshipcnt==0){
-                    nowtime=left.front();
-                }
-                else{
-                    nowtime+=t;
-                    nowship="right";
-                    for(int i=0;i<nowshipcnt;i++){
-                        arrivetime.push_back(nowtime);
-                    }
-                }
-            }
-            else if(left.empty()&&!right.empty()){
-                nowtime=right.front();
-                nowtime+=t;
-                nowship="right";
-            } 
         }
-        else if(!nowship.compare("right")){
-            if(!left.empty()&&!right.empty()){
-                while(nowtime>=right.front()){
-                    if(nowshipcnt==ship){
-                        break;
-                    }
-                    right.pop();
-                    nowshipcnt++;
-                    if(right.empty())
-                        break;
-                }
-                if(nowshipcnt==0){
-                    if(right.front()>left.front()){
-                        nowtime=left.front();
-                        nowtime+=t;
-                        nowship="left";
-                    }
-                    else{
-                        nowtime=right.front();
-                    }
-                }
-                else{
-                    nowtime+=t;
-                    nowship="left";
-                    for(int i=0;i<nowshipcnt;i++){
-                        arrivetime.push_back(nowtime);
-                    }
-                }
-            }
-            else if(left.empty()&&!right.empty()){
-                while(nowtime>=right.front()){
-                    if(nowshipcnt==ship){
-                        break;
-                    }
-                    right.pop();
-                    nowshipcnt++;
-                    if(right.empty())
-                        break;
-                }
-                if(nowshipcnt==0){
-                    nowtime=right.front();
-                }
-                else{
-                    nowtime+=t;
-                    nowship="left";
-                    for(int i=0;i<nowshipcnt;i++){
-                        arrivetime.push_back(nowtime);
-                    }
-                }
-            }
-            else if(!left.empty()&&right.empty()){
-                nowtime=left.front();
-                nowtime+=t;
-                nowship="left";
-            } 
+        else{
+
         }
     }
 
-    for(int i=0;i<arrivetime.size();i++){
-        printf("%d \n",arrivetime[i]);
-    }
+    
 
     return 0;
 }
