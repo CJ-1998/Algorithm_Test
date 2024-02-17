ef677ff6a1c8372033164a5e9689b7fc0f1b9873
CJ <choijun0627@konkuk.ac.kr>
2024-02-17 09:37:41 +0900

    Feat : Do it! 알고리즘 코딩 테스트 1문제 풀이
    8번 BOJ 1253번 좋은 수 문제 풀이 완료

--- Do_It!_Algorithm_Coding_Test_C++/8_BOJ1253.cpp
+++ Do_It!_Algorithm_Coding_Test_C++/8_BOJ1253.cpp
@@ -1,33 +1,21 @@
+/*
+문제:
+    N개의 수 중에서 어떤 수가 다른 수 두 개의 합으로 나타낼 수 있다면 그 수를 “좋다(GOOD)”고 한다.
+    N개의 수가 주어지면 그 중에서 좋은 수의 개수는 몇 개인지 출력하라.  
+    수의 위치가 다르면 값이 같아도 다른 수이다.
+
+입력:
+    첫째 줄에는 수의 개수 N(1 ≤ N ≤ 2,000), 두 번째 줄에는 i번째 수를 나타내는 Ai가 N개 주어진다. (|Ai| ≤ 1,000,000,000, Ai는 정수)
+
+출력:
+    좋은 수의 개수를 첫 번째 줄에 출력한다.
+*/
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
-
-vector<long long> sum;
-
-bool binarySearch(int num, int a, int b){
-    int start=a;
-    int end=b;
-    
-    if(start>end){
-        return false;
-    }
-    
-    int mid=(start+end)/2;
-    
-    if(sum[mid]==num){
-        return true;
-    }
-
-    if(sum[mid]>num){
-        return binarySearch(num,start,mid-1);
-    }
-
-    if(sum[mid]<num){   
-        return binarySearch(num,mid+1,end);
-    }
-}
 
 int main(){
 
@@ -42,26 +30,42 @@ int main(){
         v.push_back(temp);
     }
 
-    for(int i=0;i<cnt-1;i++){
-        for(int j=i+1;j<cnt;j++){
-            sum.push_back(v[i]+v[j]);
-        }
-    }
-
-    sort(sum.begin(),sum.end());
-    sum.erase(unique(sum.begin(),sum.end()),sum.end());
-
-    // for(int i=0;i<sum.size();i++){
-    //     cout << sum[i] <<" ";
-    // }
-    // cout<<endl;
-
+    sort(v.begin(),v.end());
 
     int answer=0;
 
     for(int i=0;i<cnt;i++){
-        if(binarySearch(v[i], 0, sum.size()-1)){
-            answer++;
+        int start=0;
+        int end=cnt-1;
+        
+        while(true){
+            // cout<<i<<" "<<start<<" "<<end<<endl;
+            if(start==end){
+                break;
+            }
+
+            if(i==start){
+                start++;
+                continue;
+            }
+            if(i==end){
+                end--;
+                continue;
+            }
+
+            if(v[start]+v[end]==v[i]){
+                answer++;
+                break;
+            }
+            if(v[start]+v[end]>v[i]){
+                end--;
+                continue;
+            }
+
+            if(v[start]+v[end]<v[i]){
+                start++;
+                continue;
+            }
         }
     }
 
