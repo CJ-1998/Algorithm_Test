11f4ec7192d0c030c5f3b991a4b6d7342afe9a6b
CJ <choijun0627@konkuk.ac.kr>
2024-03-19 07:58:28 +0900

    Feat : Do it! 알고리즘 코딩 테스트 1문제 풀이
    BOJ 1167번 트리의 지름 문제 풀이 완료

--- Do_It!_Algorithm_Coding_Test_C++/28_BOJ1167.cpp
+++ Do_It!_Algorithm_Coding_Test_C++/28_BOJ1167.cpp
@@ -1,3 +1,23 @@
+/*
+문제:
+    트리의 지름이란, 트리에서 임의의 두 점 사이의 거리 중 가장 긴 것을 말한다. 트리의 지름을 구하는 프로그램을 작성하시오.
+
+입력:
+    트리가 입력으로 주어진다. 
+    먼저 첫 번째 줄에서는 트리의 정점의 개수 V가 주어지고 (2 ≤ V ≤ 100,000)
+    둘째 줄부터 V개의 줄에 걸쳐 간선의 정보가 다음과 같이 주어진다. 
+    정점 번호는 1부터 V까지 매겨져 있다.
+    먼저 정점 번호가 주어지고, 이어서 연결된 간선의 정보를 의미하는 정수가 두 개씩 주어지는데, 
+    하나는 정점번호, 다른 하나는 그 정점까지의 거리이다. 
+    예를 들어 네 번째 줄의 경우 정점 3은 정점 1과 거리가 2인 간선으로 연결되어 있고, 
+    정점 4와는 거리가 3인 간선으로 연결되어 있는 것을 보여준다. 
+    각 줄의 마지막에는 -1이 입력으로 주어진다. 
+    주어지는 거리는 모두 10,000 이하의 자연수이다.
+
+출력:
+    첫째 줄에 트리의 지름을 출력한다.
+*/
+
 #include<iostream>
 #include<vector>
 #include<utility>
@@ -16,7 +36,6 @@ void bfs(int x){
 
     while(!q.empty()){
         int u = q.front();
-        // cout<<"u "<<u<<endl;
         q.pop();
 
         check[u]=1;
@@ -28,14 +47,8 @@ void bfs(int x){
             
             if(check[v]==1)
                 continue;
-
-            // cout<<"v "<<v<<endl;
-
-            if(sum[v]==0)
-                sum[v]=sum[u]+w;
-            else
-                sum[v]=max(sum[u]+w,sum[v]);
-            
+                
+            sum[v]=sum[u]+w;
             q.push(v);
         }
     }
@@ -43,13 +56,14 @@ void bfs(int x){
 }
 
 int main() {
+    ios_base::sync_with_stdio(true);
+    cin.tie(NULL);
+    cout.tie(NULL);
     
     int n=0;
     cin >> n;
 
     for(int i=0;i<n;i++){
-        vector<pair<int,int>>graphTemp;
-        graph.push_back(graphTemp);
         check.push_back(0);
         sum.push_back(0);
     }
@@ -57,6 +71,8 @@ int main() {
     for(int i=0;i<n;i++){
         int origin=0;
         cin>>origin;
+
+        vector<pair<int,int>>graphTemp;
 
         int temp=0;
         while(true){
@@ -67,29 +83,33 @@ int main() {
             else{
                 int weight=0;
                 cin>>weight;
-                graph[origin-1].push_back(make_pair(temp-1,weight));
+                graphTemp.push_back(make_pair(temp-1,weight));
             }
+        }
+        graph.push_back(graphTemp);
+    }
+    
+    bfs(0);
+    int maxSum=0;
+    for(int i=1;i<n;i++){
+        if(sum[maxSum]<sum[i]){
+            maxSum=i;
         }
     }
 
-    // for(int i=0;i<n;i++){
-    //     for(int j=0;j<graph[i].size();j++){
-    //         cout<<"i: "<<i+1<<" "<<graph[i][j].first+1<<" "<<graph[i][j].second<<" ";
-    //         cout<<endl;
-    //     }
-    //     cout<<endl;
-    // }
-
-    bfs(0);
-
-    // for(int i=0;i<n;i++){
-    //     cout<<sum[i]<<" ";
-    // }
-
+    fill(check.begin(),check.end(),0);
+    fill(sum.begin(),sum.end(),0);
+    bfs(maxSum);
     sort(sum.begin(),sum.end());
-
-    cout<<sum[sum.size()-1]<<endl;
-
+    cout<<sum[sum.size()-1];
 
     return 0;
 }
+
+/*
+배운 점:
+    1. 트리의 지름은 아래와 같이 찾는다. 
+        1-1. 트리에서 임의의 정점xx를 잡는다.
+        1-2. 정점xx에서 가장 먼 정점yy를 찾는다.
+        1-3. 정점yy에서 가장 먼 정점zz를 찾는다.
+*/
